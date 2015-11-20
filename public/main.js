108bcb354b9f6b9209829e5602e2fbb568daa20d
Calvin Webster <calweb@gmail.com>
2015-11-20 08:51:30 -0500

    GET TO DA CHOPPA!!

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
 var socket = require('socket.io-client')('http://localhost:3000');
 
 var commands = {
+    'get to the choppa': function (data) {
+      socket.emit('choppah:launch', {msg: "get to da choppa!"});
+    },
     'go *action': function (action) {
       console.log("action: ", action);
       socket.emit('choppah:' + action, {msg: action});
